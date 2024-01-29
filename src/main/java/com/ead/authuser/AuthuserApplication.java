7f09996daa194b7bebd349895bda83be47cbc587
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-01-28 22:11:51 -0300

    [CONFIG] - Configurações do eureka para este microserviço encontrar o Service Registry e poder se registrar

--- src/main/java/com/ead/authuser/AuthuserApplication.java
+++ src/main/java/com/ead/authuser/AuthuserApplication.java
@@ -2,8 +2,11 @@ package com.ead.authuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//sinaliza para o spring cloud que esta aplicação será um client eureka pondendo registrar a aplicação no service registry
+@EnableEurekaClient
 public class AuthuserApplication {
 
     public static void main(String[] args) {
