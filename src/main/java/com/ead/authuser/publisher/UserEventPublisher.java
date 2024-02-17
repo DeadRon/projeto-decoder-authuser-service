bba271090b4dfc333076e9af03f1d92c159034b0
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-02-17 18:05:36 -0300

    [FEATURE][SEMANA 5] - setar action type antes de mandar para a fila

--- src/main/java/com/ead/authuser/publisher/UserEventPublisher.java
+++ src/main/java/com/ead/authuser/publisher/UserEventPublisher.java
@@ -2,12 +2,14 @@ package com.ead.authuser.publisher;
 
 import com.ead.authuser.dtos.UserEventDTO;
 import com.ead.authuser.enums.ActionType;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Log4j2
 public class UserEventPublisher {
 
     @Autowired
@@ -17,8 +19,10 @@ public class UserEventPublisher {
     private String exchangeUserEvent;
 
     public void publisherUserEvent(UserEventDTO userEventDTO, ActionType actionType){
-        userEventDTO.setUserStatus(actionType.toString());
+        userEventDTO.setActionType(actionType.toString());
         rabbitTemplate.convertAndSend(exchangeUserEvent, "", userEventDTO);
+
+        log.info("[PUBLISHER] - user sended to queue {}", userEventDTO.getUserId());
     }
 
 }
