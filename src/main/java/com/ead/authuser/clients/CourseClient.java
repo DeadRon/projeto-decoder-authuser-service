1d125ae405dbd076ad9b239565e28c3bc1b8868e
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-02-20 00:29:58 -0300

    configs inicias circuit broker

--- src/main/java/com/ead/authuser/clients/CourseClient.java
+++ src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,12 +3,12 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -32,12 +32,14 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     String REQUEST_URL_COURSE;
 
+    @Retry(name = "retryInstance")
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable){
         List<CourseDto> searchResult = null;
         ResponseEntity<ResponsePageDto<CourseDto>> result = null;
         String url = REQUEST_URL_COURSE + utilsService.createUrlGetAllCoursesByUser(userId, pageable);
         log.debug("Request URL: {} ", url);
         log.info("Request URL: {} ", url);
+        System.out.println("---Start Request ao Course Microservice---");
         try{
             ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
             result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
--- src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
+++ src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
@@ -6,13 +6,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class RestTemplateConfig {
+
+    private static final int TIMIOUT = 5000;
 
     @LoadBalanced
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         // Do any additional configuration here
-        return builder.build();
+        return builder
+                .setConnectTimeout(Duration.ofMillis(TIMIOUT))
+                .setReadTimeout(Duration.ofMillis(TIMIOUT))
+                .build();
     }
 }
